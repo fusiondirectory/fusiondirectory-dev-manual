8419e9e703deb20932c624219591a0508bee81df
Côme Chilliet <come@opensides.be>
2017-09-04 17:55:55 +0200

    Fixed my name in copyright notice

--- source/conf.py
+++ source/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'FusionDirectory development'
-copyright = u'2017, Benoit Mortier Come Chilliet'
-author = u'Benoit Mortier Come Chilliet'
+copyright = u'2017, Benoit Mortier Côme Chilliet'
+author = u'Benoit Mortier Côme Chilliet'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -262,7 +262,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'FusionDirectorydevelopment.tex', u'FusionDirectory development Documentation',
-     u'Benoit Mortier Come Chilliet', 'manual'),
+     u'Benoit Mortier Côme Chilliet', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
