2ab7cede503d9ab132ece126d7629b2cca3dc176
Benoit Mortier <benoit.mortier@fusiondirectory.org>
2024-12-19 20:33:33 +0100

    :handshake: fix(authors) add new authors to the manual
    Signed-off-by: Benoit Mortier <benoit.mortier@fusiondirectory.org>

--- source/conf.py
+++ source/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'FusionDirectory development'
-copyright = u'2017, Benoit Mortier Côme Chilliet'
-author = u'Benoit Mortier Côme Chilliet'
+copyright = u'2017-2024 FusionDirectory'
+author = u'Benoit Mortier Côme Chilliet Thibault Dockx Jonathan Swaelens'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -262,7 +262,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'FusionDirectorydevelopment.tex', u'FusionDirectory development Documentation',
-     u'Benoit Mortier Côme Chilliet', 'manual'),
+     u'Benoit Mortier Côme Chilliet Thibault Dockx Jonathan Swaelens', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
